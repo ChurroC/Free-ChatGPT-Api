f2579a5d9329aa6d8734fa1196b0a51c116d4ada
churro <charanvenkatchandran@gmail.com>
2023-04-13 19:15:18 -0500

    Did a decent amoutn

--- index.js
+++ index.js
@@ -31,26 +31,36 @@ let page
             await page.waitForNavigation()
             await page.waitForSelector('input[type="email"]')
             await timeout(1000)
-            await page.type('input[type="email"]', process.env.Google_Username)
+            await page.type(
+                'input[type="email"]',
+                process.env.Google_Username || console.error('Did not set env variable "Google_Username"')
+            )
             await page.keyboard.press('Enter')
 
             await page.waitForNavigation()
             await page.waitForSelector('input[type="password"]')
             await timeout(1000)
-            await page.type('input[type="password"]', process.env.Google_Password)
+            await page.type(
+                'input[type="password"]',
+                process.env.Google_Password || console.error('Did not set env variable "Google_Password"')
+            )
             await page.keyboard.press('Enter')
 
             // Chat
             await page.waitForNavigation()
-            for (let i = 0; i < 4; i++) {
+            for (let i = 0; i < 3; i++) {
                 await page.waitForSelector('button.btn.relative.ml-auto')
+                await timeout(1000)
                 await page.click('button.btn.relative.ml-auto')
             }
 
             if (process.env.Name_Of_Chat) {
-                console.log(`//div[text()="${process.env.Name_Of_Chat}"]`)
                 await timeout(500)
-                await (await page.$x(`//div[text()="${process.env.Name_Of_Chat}"]`))[0].click()
+                try {
+                    await (await page.$x(`//div[text()="${process.env.Name_Of_Chat}"]`))[0].click()
+                } catch {
+                    console.log('Could not find chat')
+                }
             }
 
             await page.waitForNavigation()
@@ -58,27 +68,42 @@ let page
             inChat = true
         } catch (e) {
             // If it didn't work this time run it again
+            console.log(e)
             await browser.close()
             return run()
         }
     })
 })()
-//document.querySelector('label.ctp-checkbox-label>input').click()
 
-app.get('/:phrase', async (req, res) => {
+app.get('/bot', async (req, res) => {
     if (!inChat) {
         return res.status(500).send("Bot still hasn't joined the chat")
     }
+    if (!req.query.message) {
+        return res.status(400).send('You must have a message parameter')
+    }
 
-    await page.type('form>div>div>textarea', req.params.phrase)
+    await page.type('form>div>div>textarea', req.query.message)
     await page.keyboard.press('Enter')
+
     await timeout(3000)
-    res.status(200).send(
-        await page.evaluate(
-            document => [...document.querySelectorAll('div.markdown.prose.w-full.break-words')].slice(-1)[0]
-        )
-    )
+    const message = await page.evaluate(async () => {
+        let message = 0,
+            secondMessage = 1
+        while (message !== secondMessage) {
+            message = [...document.querySelectorAll('div.markdown.prose.w-full.break-words')].pop().textContent
+            await new Promise(resolve => setTimeout(resolve, 300))
+            secondMessage = [...document.querySelectorAll('div.markdown.prose.w-full.break-words')].pop().textContent
+            console.log(message)
+            console.log(secondMessage)
+            console.log(message !== secondMessage)
+        }
+        return message
+    })
+    res.status(200).send(message)
 })
+
+app.get('/regenerate', async (req, res) => {})
 
 app.listen(process.env.PORT || 8080, () => {
     console.log(`App listening at port ${process.env.PORT || 8080}`)
